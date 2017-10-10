83cf4ed026913b99223224b7608eac373743fd08
serukio <serukio@gmail.com>
2017-10-10 16:28:44 +0200

    1.2

--- www/js/main.bundle.js
+++ www/js/main.bundle.js
@@ -267,7 +267,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/hero-detail.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div *ngIf=\"hero\">\n  <h2>{{hero.name}} details!</h2>\n  <div>\n    <label>id: </label>{{hero.id}}</div>\n  <div>\n    <label>name: </label>\n    <input [(ngModel)]=\"hero.name\" placeholder=\"name\" />\n   </div>\n</div>\n<button (click)=\"goBack()\">Back</button>\n<button (click)=\"save()\">Save</button>\n<button (click)=\"callHero()\">CALL THE HERO!</button>\n"
+module.exports = "<div *ngIf=\"hero\">\n  <h2>{{hero.name}} details!</h2>\n  <div>\n    <label>id: </label>{{hero.id}}</div>\n  <div>\n    <label>name: </label>\n    <input [(ngModel)]=\"hero.name\" placeholder=\"name\" />\n   </div>\n</div>\n<button (click)=\"goBack()\">Back</button>\n<button (click)=\"save()\">Save</button>\n<button (click)=\"callHero()\">CALL THE HERO!</button>\n<button (click)=\"locateHero()\">Where's the hero!?</button>"
 
 /***/ }),
 
@@ -323,6 +323,23 @@ var HeroDetailComponent = (function () {
             console.log('Error:' + result);
         }, '872440085', true);
         // document.location.href = 'tel:872-440-085';
+    };
+    HeroDetailComponent.prototype.locateHero = function () {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        function onSuccess(position) {
+            alert('Latitude: ' + position.coords.latitude + '\n' +
+                'Longitude: ' + position.coords.longitude + '\n' +
+                'Altitude: ' + position.coords.altitude + '\n' +
+                'Accuracy: ' + position.coords.accuracy + '\n' +
+                'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+                'Heading: ' + position.coords.heading + '\n' +
+                'Speed: ' + position.coords.speed + '\n' +
+                'Timestamp: ' + position.timestamp + '\n');
+        }
+        function onError(error) {
+            alert('code: ' + error.code + '\n' +
+                'message: ' + error.message + '\n');
+        }
     };
     return HeroDetailComponent;
 }());
